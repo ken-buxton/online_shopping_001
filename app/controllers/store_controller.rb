d39eb363f462634a6c7dbde1567eb25b5489867b
Ken Buxton <kenneth.d.buxton@gmail.com>
2014-04-17 08:25:34 -0400

    V 1.020

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,18 +1,25 @@
 class StoreController < ApplicationController
   
   def index
-    # Parameter variables: :set_customer_email, :set_customer_shopping_list, :add_to_list, :delete_from_list, :commit, :shopping_list_name,
+    # Parameter variables: :set_customer_email, :set_customer_shopping_list_name, :add_to_list, :delete_from_list, :commit, :shopping_list_name,
     #   :category, :sub_category, :sub_category_group
     #   :set_change_qty_x (where x is a multi-digit number)
     # 
     # Session variables: :category, :sub_category, :sub_category_group, :customer_shopping_list_name, :customer_email
     # 
     # Make sure all session variables have a value (nil if don't exist)
-    session_vars = [:category, :sub_category, :sub_category_group, :customer_shopping_list_name, :customer_email]
+    session_vars = [:category, :sub_category, :sub_category_group, 
+      :customer_shopping_list_name, :customer_email,
+      :show_product_images
+    ]
     session_vars.each do |session_var|
       if session[session_var].nil?
         session[session_var] = ""
       end
+    end
+    
+    if session[:show_product_images] == ""
+      session[:show_product_images] == "yes"
     end
     
     # ************************************************************
@@ -37,10 +44,20 @@ class StoreController < ApplicationController
         end
       end
       
-    elsif not params[:set_customer_shopping_list].nil?
+    elsif not params[:set_customer_shopping_list_name].nil?
       # Changed current shopping list
       # Update to new shopping list
-      session[:customer_shopping_list_name] = params[:set_customer_shopping_list]
+      session[:customer_shopping_list_name] = params[:set_customer_shopping_list_name]
+      
+    elsif not params[:set_show_product_images_hidden].nil?
+      # check_box_tag will not send back a value parameter if 
+      # Changed display product image
+      # Update to new value (show/hide)
+      if params[:set_show_product_images]
+        session[:show_product_images] = "yes"
+      else
+        session[:show_product_images] = "no"
+      end
           
     elsif not params[:add_to_list].nil?
       # Added an item to the current shopping list
@@ -124,23 +141,17 @@ class StoreController < ApplicationController
         end
         
       elsif params[:commit] == "Change"
-        # logger.debug params
         params.each do |key, new_value|
           if key =~ /^set_change_qty_/
             product_id = key["set_change_qty_".size..-1].to_i
-            # logger.debug "set_change_qty_ search: #{key} = #{new_value}, p_id=#{product_id}"
             
             customer_id, customer_shopping_list = get_named_customer_shopping_info
-            # logger.debug "customer_id #{customer_id}, customer_shopping_list #{customer_shopping_list}"
             if not customer_id.nil? and not customer_shopping_list.nil?
-              # logger.debug "customer_id and customer_shopping_list not nil"
               # If item already exists, add a count of 1 to current count
               if CustomerShoppingListItem.where(customer_shopping_list_id: customer_shopping_list.id, product_id: product_id).count > 0
-                # logger.debug "Got the CustomerShoppingListItem"
                 item = CustomerShoppingListItem.where(customer_shopping_list_id: customer_shopping_list.id, product_id: product_id).first
                 item.quantity = new_value
                 item.save
-                # logger.debug "item: #{item.to_s}"
               end
             end            
           end
@@ -186,7 +197,6 @@ class StoreController < ApplicationController
     # sub_category, and sub_category_group list
     # ************************************************************
     # Determine what we'll display in the left hand margin
-    logger.debug "session[:category] = #{session[:category]}"
     conn = ActiveRecord::Base.connection
     @categories = conn.select_values(
       "select distinct category 
@@ -205,6 +215,14 @@ class StoreController < ApplicationController
         order by sub_category_group")
     @cur_category = session[:category]
     @cur_sub_category = session[:sub_category]
+    
+    # ************************************************************
+    @show_product_images = true
+    if session[:show_product_images] == "yes" 
+      @show_product_images = true
+    elsif session[:show_product_images] == "no" 
+      @show_product_images = false
+    end
     
     # ************************************************************
     # Setup the customer list
